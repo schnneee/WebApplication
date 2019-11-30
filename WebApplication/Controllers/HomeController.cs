bba15d9b6d6dfaef1685394326165c4ae47cc244
schnneee <schnneee@gmail.com>
2019-11-30 20:00:23 +0800

    feature: homewoek1130 ledger list

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Models.ViewModel;
 
 namespace WebApplication.Controllers
 {
@@ -25,6 +26,33 @@ namespace WebApplication.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult LedgerList()
+        {
+            var list = new List<LedgerViewModel>();
+            var count = 100;
+            var startDate = DateTime.Now.AddMonths(-3);
+            var random = new Random();
+
+            for (var i = 1; i <= count; i++)
+            {
+                var num = random.Next(1, 1000);
+
+                list.Add(new LedgerViewModel
+                {
+                    Date = startDate.AddMinutes(num * i),
+                    Type = (LedgerType)(num % 2),
+                    Money = (num % i + 1) * i 
+                });
+            }
+
+            list = list.OrderByDescending(x => x.Date).ToList();
+
+            ViewData["Title"] = "我的記帳本";
+
+            return View(list);
         }
     }
 }
---
+++ WebApplication/Models/ViewModel/LedgerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models.ViewModel
+{
+    public class LedgerViewModel
+    {
+        public LedgerType Type { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Money { get; set; }
+    }
+
+    public enum LedgerType
+    {
+        income = 1,
+        expend = 0
+    }
+}
