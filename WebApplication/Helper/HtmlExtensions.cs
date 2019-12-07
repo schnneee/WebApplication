21972a1d353195bc2fa74f9a5e66b36d3a4eab1a
schnneee <schnneee@gmail.com>
2019-12-07 23:58:52 +0800

    improve: add enum LedgerType display name extensions

---
+++ WebApplication/Helper/HtmlExtensions.cs
@@ -0,0 +1,22 @@
+﻿using WebApplication.Models.ViewModel;
+
+namespace WebApplication.Helper
+{
+    public static class HtmlExtensions
+    {
+        public static string GetLedgerTypeName(this LedgerType type)
+        {
+            switch (type)
+            {
+                case LedgerType.income:
+                    return "收入";
+
+                case LedgerType.expend:
+                    return "支出";
+
+                default:
+                    return "Undefined";
+            }
+        }
+    }
+}
